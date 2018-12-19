f94370013f4046936deb64f56dd50103c058395c
松尾　祐士 <gu@adominisutoretanoMacBook-puro.local>
2018-12-20 02:45:04 +0900

    add iconfont

--- src/screens/MemoDetailScreen.js
+++ src/screens/MemoDetailScreen.js
@@ -19,7 +19,7 @@ class MemoDetailScreen extends React.Component {
             講座のアイデアです。
           </Text>
         </View>
-        <CircleButton color="white" style={styles.editButton}>+</CircleButton>
+        <CircleButton color="white" style={styles.editButton} name="pencil"/>
       </View>
     );
   }
